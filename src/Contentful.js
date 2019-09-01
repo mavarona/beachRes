9c546992ebd4af8ffea8c1f9d654a6291c4378d4
mavl <mvaronamac@gmail.coml>
2019-09-02 00:11:09 +0200

    change env

--- src/Contentful.js
+++ src/Contentful.js
@@ -3,6 +3,6 @@ import {
 } from 'contentful';
 
 export default createClient({
-    space: '',
-    accessToken: ""
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
